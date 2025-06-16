f76d5bb7c6ccacd57d3338a83b559d5149d13f20
EnsarCEYHAN <ensar58588@gmail.com>
2025-06-16 22:04:50 +0300

    ex00 done

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,25 @@
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# define GREEN       "\033[32m"
+# define CYAN        "\033[36m"
+# define DARK_ORANGE "\033[38;5;166m"
+# define RED         "\033[31m"
+# define GRAY        "\033[90m"
+# define YELLOW      "\033[33m"
+# define RESET       "\033[0m"
+
+#include <algorithm>
+#include <stdexcept>
+
+template <typename T>
+typename T::iterator easyfind(T &conteiner, int value)    // bu ifade bir turdur degısken degıl !!
+{
+    typename T::iterator    it;
+    it = std::find(conteiner.begin(), conteiner.end(), value); // ilk arguman = start, hangi elemandan baslanacak aramaya. ikinci arguman = end, hangi elemana kadar aranacak. Hangi eleman degerı aranacak.
+    if (it == conteiner.end())
+        throw std::runtime_error("Value is not found \xE2\x9D\x97\xE2\x9D\x97");
+    return it;
+}
+
+#endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,12 +1,81 @@
-#include "whatever.hpp"
+#include "easyfind.hpp"
 #include <iostream>
+#include <vector>
+#include <list>
 
-int main(void)
+int main()
 {
-Awesome a(2), b(4);
-swap(a, b);
-std::cout << a << " " << b << std::endl;
-std::cout << max(a, b) << std::endl;
-std::cout << min(a, b) << std::endl;
-return (0);
+    try
+    {
+        int                 array[] = {10, 20, 30, 40, 50};
+        std::list<int>      list(array, array + 5);
+        std::vector<int>    vec(array, array + 5);
+
+        std::list<int>::iterator    l_it;
+        std::vector<int>::iterator  v_it;
+
+
+        std::cout << std::endl;
+        std::cout << GRAY << "                  :::::::::::::::::::::::::   ARRAY ELEMENTS   :::::::::::::::::::::::::" << RESET << std::endl;
+        std::cout << std::endl;
+        
+        for (int i = 0; i < 5; i++)
+            std::cout << GREEN << i + 1 << ". element of " << DARK_ORANGE << "<< array >>" << GREEN << " is = " << array[i] << RESET << std::endl;
+        std::cout << std::endl;
+
+
+        std::cout << GRAY << "                  :::::::::::::::::::::::::   LIST ELEMENTS   :::::::::::::::::::::::::" << RESET << std::endl;
+        std::cout << std::endl;
+        
+        int i = 0;
+        for (l_it = list.begin(); l_it != list.end(); l_it++)
+        {
+            std::cout << GREEN << i + 1 << ". element of " << DARK_ORANGE << "<< list >>" << GREEN << " is = " << *l_it << RESET << std::endl;
+            i++;
+        }
+
+        std::cout << std::endl;
+        l_it = easyfind(list, 10);
+        std::cout << GREEN << *l_it << " is founded in " << DARK_ORANGE << "<< list >>" << RESET << std::endl;
+        l_it = easyfind(list, 20);
+        std::cout << GREEN << *l_it << " is founded in " << DARK_ORANGE << "<< list >>" << RESET << std::endl;
+        l_it = easyfind(list, 30);
+        std::cout << GREEN << *l_it << " is founded in " << DARK_ORANGE << "<< list >>" << RESET << std::endl;
+        l_it = easyfind(list, 40);
+        std::cout << GREEN << *l_it << " is founded in " << DARK_ORANGE << "<< list >>" << RESET << std::endl;
+        l_it = easyfind(list, 50);
+        std::cout << GREEN << *l_it << " is founded in " << DARK_ORANGE << "<< list >>" << RESET << std::endl;
+        std::cout << std::endl;
+
+
+        std::cout << GRAY << "                  :::::::::::::::::::::::::   VECTOR ELEMENTS   :::::::::::::::::::::::::" << RESET << std::endl;
+        std::cout << std::endl;
+
+        i = 0;
+        for (v_it = vec.begin(); v_it != vec.end(); v_it++)
+        {
+            std::cout << GREEN << i + 1 << ". element of " << DARK_ORANGE << "<< vec >>" << GREEN << " is = " << *v_it << RESET << std::endl;
+            i++;
+        }
+
+        std::cout << std::endl;
+        v_it = easyfind(vec, 10);
+        std::cout << GREEN << *v_it << " is founded in " << DARK_ORANGE << "<< vector >>" << RESET << std::endl;
+        v_it = easyfind(vec, 20);
+        std::cout << GREEN << *v_it << " is founded in " << DARK_ORANGE << "<< vector >>" << RESET << std::endl;
+        v_it = easyfind(vec, 30);
+        std::cout << GREEN << *v_it << " is founded in " << DARK_ORANGE << "<< vector >>" << RESET << std::endl;
+        v_it = easyfind(vec, 40);
+        std::cout << GREEN << *v_it << " is founded in " << DARK_ORANGE << "<< vector >>" << RESET << std::endl;
+        v_it = easyfind(vec, 50);
+        std::cout << GREEN << *v_it << " is founded in " << DARK_ORANGE << "<< vector >>" << RESET << std::endl;
+        std::cout << std::endl;
+    }
+
+    catch (std::exception &e)
+    {
+        std::cerr << RED << e.what() << RESET << std::endl;
+    }
+
+
 }
